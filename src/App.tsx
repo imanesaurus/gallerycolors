d83c8535de00a31b5e6842977587e714613139a9
imanesaurus <pirmansah.otoluwa@gmail.com>
2021-02-06 10:54:58 +0700

    make 5x8 gridview

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
+import React from "react";
 import "./App.css";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
+const MARGIN = 20;
 
 function App() {
+  const [colors, setColor] = React.useState([""]);
+
+  function exportColor() {
+    let colorArray = [];
+    for (let c = 0; c < 40; c++) {
+      colorArray.push(randomColorRgb());
+    }
+    setColor(colorArray);
+  }
+  React.useEffect(() => {
+    exportColor();
+    console.log("Color", colors);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Gallery App</p>
-        <div className="App-color-components" style={styles.container}></div>
+        <div className="App-text">
+          <p>Gallery App</p>
+        </div>
+        <div className="App-header-container">
+          {colors.map((color) => {
+            return (
+              <div className="App-color-components">
+                <div
+                  className="App-color-image"
+                  style={{
+                    ...styles.image,
+                    backgroundColor: color,
+                    marginRight: MARGIN / 4,
+                    marginBottom: MARGIN / 4,
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
       </header>
     </div>
   );
@@ -22,10 +56,10 @@ function randomColorRgb() {
 }
 
 const styles = {
-  container: {
-    backgroundColor: randomColorRgb(),
-    width: width / 5,
-    height: height / 8,
+  image: {
+    marginHorizontal: MARGIN,
+    width: width / 5 - MARGIN,
+    height: height / 9 - MARGIN / 4,
   },
 };
 
