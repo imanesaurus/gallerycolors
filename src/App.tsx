3916ce70d31c96aa0ba117797276170414e6e475
imanesaurus <pirmansah.otoluwa@gmail.com>
2021-02-06 09:31:37 +0700

    add random color function and displaying it

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Gallery App</p>
+        <div className="App-color-components" style={styles.container}></div>
       </header>
     </div>
   );
 }
+
+function randomColorRgb() {
+  const red = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${blue}, ${green})`;
+}
+
+const styles = {
+  container: {
+    backgroundColor: randomColorRgb(),
+    width: width / 5,
+    height: height / 8,
+  },
+};
 
 export default App;
